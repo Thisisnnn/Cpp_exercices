3794f4c7a2cf68436823d7c39a5b6f417f1fe631
Thisisnnn <kyhia.tran@oteria.fr>
2026-03-17 18:26:22 +0100

    Finally able to read a file

--- strings_cpp.cpp
+++ strings_cpp.cpp
@@ -7,9 +7,7 @@ int main () {
         ifstream file("test.txt");
         string line;
 
-        // Use a while loop together with the getline() function to read the file line by line
         while (getline (file, line)) {
-        // Output the text from the file
         cout << line;
 
         file.close();
