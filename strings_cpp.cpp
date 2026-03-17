8530036efe492be3f4d7663eb9d85ba4bd35ae4d
Thisisnnn <kyhia.tran@oteria.fr>
2026-03-17 23:10:49 +0100

    Only print if there's minimum three ASCII characters in a row

--- strings_cpp.cpp
+++ strings_cpp.cpp
@@ -4,14 +4,31 @@
 using namespace std;
 
 int main () {
-        ifstream file("test.txt");
         string line;
-
+        string characters;
+        ifstream file("test.txt");
+        if (!file) {  
+                cout<<"File doesn't exist.";          
+        }
+        else{
         while (getline (file, line)) {
                 for (char c : line){
-                        if ( 32 < int(c) &&  int(c) < 126){
-                                std::cout << c;
+                        if ( 32 < int(c) && int(c) < 126 ){
+                                characters += c;
+                                }
+                        else{
+                                if (characters.length() > 3){
+                                        std::cout << characters << "\n";
+                                        characters.clear();
+                        }
                         }
                 }
+        if (characters.length() > 3){
+                std::cout << characters << "\n";
+                characters.clear();
         file.close();
-}}
+        return 0;
+        }
+        }
+}
+}
