1428ab03df2613142323729f17146b58674a58e6
Thisisnnn <kyhia.tran@oteria.fr>
2026-03-24 18:30:59 +0100

    Practice Book: Exercice 1 - Finished

---
+++ Second_day/Practice_Workbook/Exercice_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iterator>
+#include <ostream>
+#include <string>
+
+int main(){
+    int age;
+    std::string name;
+    std::cout << "Enter your name: ";
+    std::cin >> name;
+
+    std::cout << "Enter your age: ";
+    std::cin >> age;
+    
+    std::cout << "Hello " << name << ", you are " << age << " years old." << std::endl;
+    return 0;
+}
