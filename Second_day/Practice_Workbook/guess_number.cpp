303c39607cd781e501cf2e58aa1459bd635c4eb0
Thisisnnn <kyhia.tran@oteria.fr>
2026-03-25 12:41:01 +0100

    Practice Book: Exercice 4 - Added a bonus -> limit of guesses

--- Second_day/Practice_Workbook/guess_number.cpp
+++ Second_day/Practice_Workbook/guess_number.cpp
@@ -2,16 +2,35 @@
 #include <cstdlib>
 #include <iostream>
 #include <ctime>
+#include <ostream>
 
 int main(){
     srand(time(0));
     int number = rand() % 101; //Generate a number between 1 and 100
+    std::cout << number << std::endl;
     int guess;
     int attempts;
+    char choice;
+    int limit;
 
     std::cout << "Guess the number (between 1 and 100)!" << std::endl;
+    std::cout << "Do you want to set a limit? (y/n) ";
+    std::cin >> choice;
 
-    while (1) {
+    switch (choice) {
+        case 'y':
+            std::cout << "What's the limit? ";
+            std::cin >> limit;
+            break;
+        case 'n':
+            break;
+        default:
+            std::cout << "Incorrect answer." << std::endl;
+            break;
+            return 1;
+    }
+
+    while (limit > attempts) {
         std::cout << "Your guess: ";
         std::cin >> guess;
 
@@ -25,8 +44,9 @@ int main(){
         }
         else {
             std::cout << "Congratulation!\nYou found it in " << attempts << " attempts!" << std::endl;
-            break;
+            return 0;         
         }
     }
+    std::cout << "You lost D:" << std::endl;
     return 0; 
 }
