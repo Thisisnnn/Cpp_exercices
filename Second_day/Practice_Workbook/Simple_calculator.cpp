a555188c49225afdd489b813ac68b02746b727ee
Thisisnnn <kyhia.tran@oteria.fr>
2026-03-25 11:35:27 +0100

    Practice Book: Exercice 2 - Finished

---
+++ Second_day/Practice_Workbook/Simple_calculator.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <ostream>
+
+int main(){
+    int first_number;
+    int second_number;
+    char operators;
+    int result;
+    std::cout << "Enter the first number: ";
+    std::cin >> first_number;
+    std::cout << "Enter the second number: ";
+    std::cin >> second_number;
+    std::cout << "Enter the operator: ";
+    std::cin >> operators;
+
+    switch (operators) {
+        case '+':
+            result = first_number + second_number;
+            break;
+        case '-':
+            result = first_number - second_number;
+            break;
+        case '*':
+            result = first_number * second_number;
+            break;
+        case '/':
+            if (second_number != 0) { // Avoid division by zero
+                result = first_number / second_number;
+            }
+            else {
+                std::cout << "Disivion by zero impossible." << std::endl;
+            }
+            break;
+        default:
+            std::cout << "Invalid Operator" << std::endl;
+            return 1;
+    }
+    std::cout << "Result: " << first_number << operators << second_number << " = " << result << std::endl;
+    return 0;
+}
